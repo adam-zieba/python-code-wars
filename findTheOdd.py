ebd15334ffde012f83b937949e93b637b1c47c66
Adam Zięba <adam.zieba@swiatlowodotwarty.pl>
2023-06-16 12:38:36 +0200

    Add Find the Odd solution

---
+++ findTheOdd.py
@@ -0,0 +1,13 @@
+def find_it(seq):
+    for i in seq:
+        if (seq.count(i) % 2 != 0):
+          return i
+
+
+
+def find_it_best(seq):
+    for i in seq:
+        if (seq.count(i) % 2 != 0):
+          return i
+
+find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
