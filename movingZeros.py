0d7289e3445806c1eca67400493f076b4623d91c
Adam Zięba <adam.zieba@swiatlowodotwarty.pl>
2023-11-22 13:27:11 +0100

    add links to the movingZeros.py solution

--- movingZeros.py
+++ movingZeros.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/52597aa56021e91c93000cb0/python
+
 def move_zeros(lst):
     newList = []
     howManyZeroes = lst.count(0)
@@ -12,6 +14,10 @@ def move_zeros(lst):
     return newList
 
 
+# https://www.codewars.com/kata/reviews/57cb52c733c32f4c48000076/groups/57cb983f58da9ee25b000126
+def move_zeros_best(arr):
+    l = [i for i in arr if isinstance(i, bool) or i!=0]
+    return l+[0]*(len(arr)-len(l))
 
 
 
